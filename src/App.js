7d0de3585e36c758bff92f360edcdc259934e35c
Tien Le <tiendev37@gmail.com>
2023-06-10 13:15:24 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="app">Lê Văn Tiến</div>;
+  return <div className="app">Lê Văn Tiến - Cr7</div>;
 }
 
 export default App;
